0594b8b4b5aa98575d002c2b8325c035d2702ab5
Alexander Laird <alaird@twilio.com>
2018-03-28 11:28:54 -0700

    Adding certbot for SSL in deployment.

--- deploy/scripts/install_apache_dependencies.sh
+++ deploy/scripts/install_apache_dependencies.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-apt-get install -y vim apache2 libapache2-mod-wsgi-py3
+apt-get install software-properties-common
+add-apt-repository ppa:certbot/certbot
+apt-get update
+
+apt-get install -y vim apache2 libapache2-mod-wsgi-py3 python-certbot-apache
 
 a2enmod wsgi ssl
 
@@ -9,4 +13,7 @@ mkdir -p /var/twiltwil/log/
 
 sudo cp /srv/twiltwil/deploy/conf/django.conf /etc/apache2/sites-available/twiltwil.conf
 
+a2dissite 000-default default-ssl
 a2ensite twiltwil
+
+certbot --apache --authenticator standalone --installer apache --non-interactive --agree-tos --email ${TWILTWIL_ADMIN_EMAIL} --domains twiltwil.alexlaird.com
