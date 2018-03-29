ed9eb264970c1474f3c920f25d78fd892640c6ca
Alex Laird <alexdlaird@gmail.com>
2018-03-29 12:46:59 -0700

    Moving reservation.accepted to workspace callback.

--- twiltwil/api/views/webhooktaskrouterworkflowview.py
+++ twiltwil/api/views/webhooktaskrouterworkflowview.py
@@ -4,9 +4,6 @@ import logging
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from twiltwil.api.models import Message
-from twiltwil.api.utils import messageutils
-
 __author__ = 'Alex Laird'
 __copyright__ = 'Copyright 2018, Alex Laird'
 __version__ = '0.1.0'
@@ -17,18 +14,5 @@ logger = logging.getLogger(__name__)
 class WebhookTaskRouterWorkflowView(APIView):
     def post(self, request, *args, **kwargs):
         logger.info('Workflow POST received: {}'.format(json.dumps(request.data)))
-
-        if 'EventType' in request.data:
-            if request.data['EventType'] == 'reservation.accepted':
-                logger.info('Processing reservation.accepted')
-
-                task_attributes = json.loads(messageutils.cleanup_json(request.data['TaskAttributes']))
-
-                for message in Message.objects.for_channel('sms').inbound().for_number(
-                        task_attributes['from']).no_worker().iterator():
-                    message.task_sid = request.data['TaskSid']
-                    message.worker_sid = request.data['WorkerSid']
-
-                    message.save()
 
         return Response()
--- twiltwil/api/views/webhooktaskrouterworkspaceview.py
+++ twiltwil/api/views/webhooktaskrouterworkspaceview.py
@@ -4,6 +4,9 @@ import logging
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from twiltwil.api.models import Message
+from twiltwil.api.utils import messageutils
+
 __author__ = 'Alex Laird'
 __copyright__ = 'Copyright 2018, Alex Laird'
 __version__ = '0.1.0'
@@ -14,5 +17,18 @@ logger = logging.getLogger(__name__)
 class WebhookTaskRouterWorkspaceView(APIView):
     def post(self, request, *args, **kwargs):
         logger.info('Workspace POST received: {}'.format(json.dumps(request.data)))
+
+        if 'EventType' in request.data:
+            if request.data['EventType'] == 'reservation.accepted':
+                logger.info('Processing reservation.accepted')
+
+                task_attributes = json.loads(messageutils.cleanup_json(request.data['TaskAttributes']))
+
+                for message in Message.objects.for_channel('sms').inbound().for_number(
+                        task_attributes['from']).no_worker().iterator():
+                    message.task_sid = request.data['TaskSid']
+                    message.worker_sid = request.data['WorkerSid']
+
+                    message.save()
 
         return Response()
