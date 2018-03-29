2c15dc7c5f0a172d9d3bdb2723d45e20e7e3ddff
Alexander Laird <alaird@twilio.com>
2018-03-28 18:33:29 -0700

    Attempting to fix build/deploy.

--- deploy/scripts/install_os_dependencies.sh
+++ deploy/scripts/install_os_dependencies.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y vim htop bash-completion make wget build-essential
+sudo apt-get install -y vim htop bash-completion make wget build-essential mysql-client-5.7
--- deploy/scripts/install_python_dependencies.sh
+++ deploy/scripts/install_python_dependencies.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y vim python3 python3-pip python3-dev python3-mysqldb
+sudo apt-get install -y vim python3 python3-pip python3-dev python3-mysqldb libmysqlclient-dev
