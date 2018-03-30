2a0c97475fdec1a4bf2c1fd453ce3d1bfc599134
Alex Laird <alexdlaird@gmail.com>
2018-03-29 19:25:45 -0700

    Adding mock to fix broken test.

--- twiltwil/auth/tests/testcaseauthviews.py
+++ twiltwil/auth/tests/testcaseauthviews.py
@@ -47,7 +47,8 @@ class TestCaseAuthViews(TwilTwilTestCase):
         self.assertContains(response, 'invalid is not one of the available choices')
 
     @mock.patch('twiltwil.auth.services.authservice.twilioauthservice.delete_worker')
-    def test_logout_success(self, mock_delete_worker):
+    @mock.patch('twiltwil.auth.services.authservice.twilioauthservice.delete_chat_user')
+    def test_logout_success(self, mock_delete_chat_user, mock_delete_worker):
         # GIVEN
         userhelper.given_a_user_exists_and_is_logged_in(self.client)
 
