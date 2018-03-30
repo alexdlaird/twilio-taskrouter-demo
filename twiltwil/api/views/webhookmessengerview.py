f53175044b0cb99a3ba37c2ae0291ad63f57a970
Alex Laird <alexdlaird@gmail.com>
2018-03-30 14:00:15 -0700

    Adding GET method for Messenger webhook verification.

--- twiltwil/api/views/webhookmessengerview.py
+++ twiltwil/api/views/webhookmessengerview.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,6 +13,14 @@ logger = logging.getLogger(__name__)
 
 
 class WebhookMessengerView(APIView):
+    def get(self, request, *args, **kwargs):
+        token_sent = request.args.get("hub.verify_token")
+
+        if token_sent == settings.MESSENGER_VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+
+        return 'Invalid verification token'
+
     def post(self, request, *args, **kwargs):
         logger.info('Messenger POST received: {}'.format(json.dumps(request.data)))
 
