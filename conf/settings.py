72987756ea0e2faf8545c6add1060a8c7cc70ee7
Alex Laird <alexdlaird@gmail.com>
2019-01-06 16:20:16 -0800

    no message

--- conf/settings.py
+++ conf/settings.py
@@ -4,8 +4,8 @@ This generic settings builder reads the appropriate configuration file for diffe
 Note that the system environment variable ENVIRONMENT should be set to a slug that matches the deployed environment.
 
 * If ENVIRONMENT is set to `dev`, `dev.py` will be used for configuration, using values from `.env`
-* If ENVIRONMENT is not 'dev', `deploy.py` will be used for configuration, using system environment variables
-* If `test` is passed as an argument, ENVIRONMENT is ignored and `test.py` is used for configuration, reading from .env`
+* If ENVIRONMENT is not `dev', `deploy.py` will be used for configuration, using system environment variables
+* If `test` is passed as an argument, ENVIRONMENT is ignored and `test.py` is used for configuration, reading from `.env`
 
 All configuration files first read `common.py` before applying deployment-specific configurations.
 """
