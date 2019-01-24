124be03137900b2f5a0225e15b9fdad0098e7297
Alex Laird <alexdlaird@gmail.com>
2019-01-23 20:15:57 -0800

    Adding the full suite of webhooks Twilio uses for this project to support pyngrok when in development mode.

--- twiltwil/common/apps.py
+++ twiltwil/common/apps.py
@@ -16,25 +16,46 @@ class CommonConfig(AppConfig):
     verbose_name = 'Common'
 
     def ready(self):
-        if os.environ.get('RUN_MAIN', None) and \
-                settings.DEV_SERVER and settings.USE_NGROK:
-            CommonConfig.init_ngrok()
+        if os.environ.get('RUN_MAIN', None) and settings.DEV_SERVER and settings.USE_NGROK:
+            # pyngrok will only be installed, and should only ever be initialized, in a dev environment
+            from pyngrok import ngrok
+
+            # Get the dev server port (defaults to 8000 for Django, can be overriden with the
+            # last arg when calling `runserver`)
+            addrport = urlparse('http://{}'.format(sys.argv[-1]))
+            port = addrport.port if addrport.netloc and addrport.port else 8000
+
+            # Open a ngrok tunnel to the dev server
+            public_url = ngrok.connect(port).rstrip("/")
+            print('ngrok tunnel "{}" -> "http://127.0.0.1:{}/"'.format(public_url, port))
+
+            # Update any base URLs or webhooks to use the public ngrok URL
+            settings.PROJECT_HOST = public_url
+            CommonConfig.init_webhooks(public_url)
 
     @staticmethod
-    def init_ngrok():
-        from pyngrok import ngrok
-
-        addrport = urlparse('http://{}'.format(sys.argv[-1]))
-        port = addrport.port if addrport.netloc and addrport.port else 8000
-        public_url = ngrok.connect(port)
-        print('ngrok tunnel "{}" -> "http://127.0.0.1:{}/"'.format(public_url, port))
-
-        settings.PROJECT_HOST = public_url.rstrip("/")
-
-        CommonConfig.init_webhooks(public_url)
-
-    @staticmethod
-    def init_webhooks(callback_url):
+    def init_webhooks(public_url):
         client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-        phone_number_sid = client.incoming_phone_numbers.list(phone_number=settings.TWILIO_SMS_FROM)[0].sid
-        client.incoming_phone_numbers(phone_number_sid).update(sms_url=callback_url)
+
+        phone_number = client.incoming_phone_numbers.list(phone_number=settings.TWILIO_SMS_FROM)[0]
+        sms_callback_url = "{}/api/webhooks/sms".format(public_url)
+        voice_callback_url = "{}/api/webhooks/voice".format(public_url)
+        client.incoming_phone_numbers(phone_number.sid).update(voice_url=voice_callback_url,
+                                                               sms_url=sms_callback_url)
+
+        resource_name = 'twiltwil_' + os.environ.get('ENVIRONMENT')
+
+        post_webhook_url = "{}/api/webhooks/chat/event".format(public_url)
+        for service in client.chat.services.list():
+            if service.friendly_name == resource_name:
+                client.chat.services(service.sid).update(post_webhook_url=post_webhook_url)
+
+        event_callback_url = "{}/api/webhooks/taskrouter/workspace".format(public_url)
+        workspaces = client.taskrouter.workspaces.list(friendly_name=resource_name)
+        if len(workspaces) > 0:
+            client.taskrouter.workspaces(workspaces[0].sid).update(event_callback_url=event_callback_url)
+
+            assignment_callback_url = "{}/api/webhooks/taskrouter/workflow".format(public_url)
+            for workflow in client.taskrouter.workspaces(workspaces[0].sid).workflows.list():
+                client.taskrouter.workspaces(workspaces[0].sid).workflows(workflow.sid).update(
+                    assignment_callback_url=assignment_callback_url)
