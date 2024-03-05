cee1cdc6b3d367ae52e1309df41515078018078b
Alex Laird <contact@alexlaird.com>
2024-03-05 12:27:27 -0600

    Build improvements.

--- twiltwil/common/apps.py
+++ twiltwil/common/apps.py
@@ -15,7 +15,7 @@ class CommonConfig(AppConfig):
     verbose_name = 'Common'
 
     def ready(self):
-        if settings.DEV_SERVER and settings.USE_NGROK:
+        if settings.USE_NGROK and os.environ.get("NGROK_AUTHTOKEN"):
             # pyngrok will only be installed, and should only ever be initialized, in a dev environment
             from pyngrok import ngrok
 
